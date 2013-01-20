97ca60e2a0a4b4a08242d15bc84e7cd53cbb0525
psandoval-fsl <b20593@freescale.com>
2013-01-19 22:54:46 -0500

    initial commit

---
+++ src/com/pigote/objectpool/SceneManager.java
@@ -0,0 +1,57 @@
+package com.pigote.objectpool;
+
+import org.andengine.engine.Engine;
+
+import com.pigote.objectpool.BaseScene;
+
+public class SceneManager
+{
+    //---------------------------------------------
+    // VARIABLES
+    //---------------------------------------------
+    
+    private static final SceneManager INSTANCE = new SceneManager();
+    
+    private SceneType currentSceneType = SceneType.SCENE_SPLASH;
+    
+    private BaseScene currentScene;
+    
+    private Engine engine = ResourcesManager.getInstance().engine;
+    
+    public enum SceneType
+    {
+        SCENE_SPLASH,
+        SCENE_MENU,
+        SCENE_GAME,
+    }
+    
+    //---------------------------------------------
+    // CLASS LOGIC
+    //---------------------------------------------
+    
+    public void setScene(BaseScene scene)
+    {
+        engine.setScene(scene);
+        currentScene = scene;
+        currentSceneType = scene.getSceneType();
+    }
+    
+    //---------------------------------------------
+    // GETTERS AND SETTERS
+    //---------------------------------------------
+    
+    public static SceneManager getInstance()
+    {
+        return INSTANCE;
+    }
+    
+    public SceneType getCurrentSceneType()
+    {
+        return currentSceneType;
+    }
+    
+    public BaseScene getCurrentScene()
+    {
+        return currentScene;
+    }
+}
---
+++ src/com/pigote/objectpool/SplashScene.java
@@ -0,0 +1,49 @@
+package com.pigote.objectpool;
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.opengl.util.GLState;
+
+import com.pigote.objectpool.SceneManager.SceneType;
+import com.pigote.objectpool.BaseScene;
+
+public class SplashScene extends BaseScene{
+
+	private Sprite splash;
+	
+	@Override
+	public void createScene() {
+		
+		splash = new Sprite(0, 0, resourcesManager.splash_region, vbom){
+			@Override
+		    protected void preDraw(GLState pGLState, Camera pCamera){
+		       super.preDraw(pGLState, pCamera);
+		       pGLState.enableDither();
+		    }
+		};
+		
+		splash.setPosition(480, 270);
+		attachChild(splash);
+	}
+
+	@Override
+	public void onBackKeyPressed() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public SceneType getSceneType() {
+		
+		return SceneType.SCENE_SPLASH;
+	}
+
+	@Override
+	public void disposeScene() {
+		splash.detachSelf();
+		splash.dispose();
+		this.detachSelf();
+		this.dispose();		
+	}
+
+}
